db5bada875f541c060508a66a98a4d9a3424a572
raver119@gmail.com <raver119@gmail.com>
2019-04-20 20:05:12 +0300

    one more container

--- docker/spark/docker-build.sh
+++ docker/spark/docker-build.sh
@@ -6,3 +6,4 @@ docker build -t spark-base:2.3.3 ./docker/base
 docker build -t spark-master:2.3.3 ./docker/spark-master
 docker build -t spark-worker:2.3.3 ./docker/spark-worker
 docker build -t spark-submit:2.3.3 ./docker/spark-submit
+docker build -t spark-dl4j:latest ./docker/spark-dl4j
