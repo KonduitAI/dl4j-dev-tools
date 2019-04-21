24fd352514ab36fdd22e16543f4c9149bb070d5b
raver119@gmail.com <raver119@gmail.com>
2019-04-21 18:07:26 +0300

    route stdout to file

--- docker/spark/docker/spark-worker/start-worker.sh
+++ docker/spark/docker/spark-worker/start-worker.sh
@@ -7,6 +7,6 @@ mkdir -p $SPARK_WORKER_LOG
 
 export SPARK_HOME=/spark
 
-ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
+# ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
 
-/spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
+/spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/$SPARK_LOCAL_IP.out
