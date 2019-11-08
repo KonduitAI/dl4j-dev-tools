a7716e2ceceec46ca9cf19f751c98998e196ac3e
Alex Black <blacka101@gmail.com>
2019-11-08 13:38:31 +1100

    Fixes
    Signed-off-by: Alex Black <blacka101@gmail.com>

--- docker/spark/docker-build.sh
+++ docker/spark/docker-build.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-docker build -t spark-base:2.3.3 ./docker/base
-docker build -t spark-master:2.3.3 ./docker/spark-master
-docker build -t spark-worker:2.3.3 ./docker/spark-worker
-docker build -t spark-submit:2.3.3 ./docker/spark-submit
+docker build -t spark-base:2.4.4 ./docker/base
+docker build -t spark-master:2.4.4 ./docker/spark-master
+docker build -t spark-worker:2.4.4 ./docker/spark-worker
+docker build -t spark-submit:2.4.4 ./docker/spark-submit
 docker build -t spark-dl4j:latest ./docker/spark-dl4j
