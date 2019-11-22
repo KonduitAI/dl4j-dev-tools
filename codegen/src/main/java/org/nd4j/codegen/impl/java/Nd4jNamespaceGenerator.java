0bc29120e9f16f57011aba98cf890c41952b9dda
Paul Dubs <paul.dubs@gmail.com>
2019-11-22 09:46:15 +0100

    Refactor Nd4jNamespaceGenerator
    Signed-off-by: Paul Dubs <paul.dubs@gmail.com>

--- codegen/src/main/java/org/nd4j/codegen/impl/java/Nd4jNamespaceGenerator.java
+++ codegen/src/main/java/org/nd4j/codegen/impl/java/Nd4jNamespaceGenerator.java
@@ -4,6 +4,7 @@ import com.squareup.javapoet.CodeBlock;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
+import org.jetbrains.annotations.NotNull;
 import org.nd4j.base.Preconditions;
 import org.nd4j.codegen.api.*;
 import org.nd4j.codegen.api.doc.DocSection;
@@ -22,6 +23,27 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Nd4jNamespaceGenerator {
+    private static Map<DataType, Class> typeMapping = new HashMap<>();
+    private static Map<DataType, Class> arrayTypeMapping = new HashMap<>();
+    private static Map<DataType, String> validationMapping = new HashMap<>();
+    private static Count exactlyOne = new Exactly(1);
+
+    static {
+        typeMapping.put(DataType.BOOL, boolean.class);
+        typeMapping.put(DataType.FLOATING_POINT, double.class);
+        typeMapping.put(DataType.NUMERIC, double.class);
+        typeMapping.put(DataType.INT, long.class);
+
+        arrayTypeMapping.put(DataType.BOOL, boolean[].class);
+        arrayTypeMapping.put(DataType.FLOATING_POINT, double[].class);
+        arrayTypeMapping.put(DataType.NUMERIC, double[].class);
+        arrayTypeMapping.put(DataType.INT, long[].class);
+
+        validationMapping.put(DataType.BOOL, "validateBool");
+        validationMapping.put(DataType.FLOATING_POINT, "validateFloatingPoint");
+        validationMapping.put(DataType.NUMERIC, "validateNumerical");
+        validationMapping.put(DataType.INT, "validateInteger");
+    }
 
     private static ConstraintCodeGenerator constraintCodeGenerator = new JavaConstraintCodeGenerator();
 
@@ -33,34 +55,15 @@ public class Nd4jNamespaceGenerator {
 
         TypeSpec.Builder builder = TypeSpec.classBuilder(className)
                 .addModifiers(Modifier.PUBLIC);
-
-        //Add private no-arg constructor
-        MethodSpec noArg = MethodSpec.constructorBuilder()
-                .addModifiers(Modifier.PRIVATE)
-                .build();
-
-        builder.addMethod(noArg);
+        addDefaultConstructor(builder);
 
         //Add ops
-        List<Op> ops = new ArrayList<>();
-        for(Op o : namespace.getOps()){
-            if(o.isAbstract())
-                continue;
-            ops.add(o);
-        }
+        namespace.getOps()
+                .stream()
+                .filter(it -> !it.isAbstract())
+                .sorted(Comparator.comparing(Op::getOpName))
+                .forEachOrdered(o -> builder.addMethod(creatorMethod(o)));
 
-        //Also add includes:
-        List<String> include = namespace.getInclude();
-        if(include != null && !include.isEmpty()){
-            throw new UnsupportedOperationException("Handling generation with includes not yet supported");
-        }
-
-        Collections.sort(ops, Comparator.comparing(Op::getOpName));
-
-        //Add ops:
-        for(Op o : ops){
-            builder.addMethod(creatorMethod(o));
-        }
 
         TypeSpec ts = builder.build();
 
@@ -71,25 +74,32 @@ public class Nd4jNamespaceGenerator {
         jf.writeTo(directory);
     }
 
+    private static void addDefaultConstructor(TypeSpec.Builder builder) {
+        //Add private no-arg constructor
+        MethodSpec noArg = MethodSpec.constructorBuilder()
+                .addModifiers(Modifier.PRIVATE)
+                .build();
+
+        builder.addMethod(noArg);
+    }
+
     private static MethodSpec creatorMethod(Op op){
         MethodSpec.Builder c = MethodSpec.methodBuilder(GenUtil.ensureFirstIsNotCap(op.getOpName()))
                 .addModifiers(Modifier.PUBLIC, Modifier.STATIC);
 
-        if(op.getArgs() != null && !op.getArgs().isEmpty()){
-            final Arg lastArg = op.getArgs().get(op.getArgs().size() - 1);
-            if(lastArg.getCount() != null && !(lastArg.getCount() instanceof Exactly && ((Exactly) lastArg.getCount()).getCount() == 1)){
-                c.varargs(true);
-            }
-        }
+        enableVarargsOnLastArg(c, op);
 
-        boolean singleOut = op.getOutputs().size() == 1;
-        if(singleOut){
-            c.returns(INDArray.class);
-        } else {
-            c.returns(INDArray[].class);
-        }
+        buildJavaDoc(op, c);
+        List<String> inNames = buildParameters(c, op);
+        buildConstraints(c, op);
+        buildExecution(c, op, inNames);
+
+        return c.build();
+    }
 
 
+
+    private static void  buildJavaDoc(Op op, MethodSpec.Builder c) {
         //Method javadoc:
         List<DocSection> doc = op.getDoc();
         if(doc != null && !doc.isEmpty()){
@@ -121,83 +131,21 @@ public class Nd4jNamespaceGenerator {
             c.addJavadoc("\n");
         }
 
-        //Inputs:
         List<Input> in = op.getInputs();
-        List<String> inNames = new ArrayList<>();
         if(in != null && !in.isEmpty()){
-            Map<DataType, String> validationMapping = new HashMap<>();
-            validationMapping.put(DataType.BOOL, "validateBool");
-            validationMapping.put(DataType.FLOATING_POINT, "validateFloatingPoint");
-            validationMapping.put(DataType.NUMERIC, "validateNumerical");
-            validationMapping.put(DataType.INT, "validateInteger");
-
             for(Input i : in){
-//                inNames.add(i.getName());
-
-                if( i.getCount() == null || (i.getCount() instanceof Exactly && ((Exactly) i.getCount()).getCount() == 1)) {
-                    //Single input
-                    c.addParameter(INDArray.class, i.getName());
-                } else {
-                    //Array input
-                    c.addParameter(INDArray[].class, i.getName());
-                }
                 c.addJavadoc("@param " + i.getName() + " " + (i.getDescription() == null ? "" : DocTokens.processDocText(i.getDescription(), op)) + " (" + i.getType() + " type)\n");
-                // Check for parameter types
-                c.addStatement(CodeBlock.of("$T.$L($S, $L)", NDValidation.class, validationMapping.get(i.getType()), op.getOpName(), i.getName()));
-                // Check for parameter counts
-                if(i.getCount() != null && !(i.getCount() instanceof Exactly && ((Exactly) i.getCount()).getCount() == 1)){
-                    final Count count = i.getCount();
-                    final String errorMessage = i.getName() + " has incorrect size/length. Expected: " + count.toString() + ", got %s";
-                    if(count instanceof Exactly){
-                        c.addStatement(CodeBlock.of("$T.checkArgument($L.length == $L, $S, $L)", Preconditions.class, i.getName(), ((Exactly) count).getCount(), errorMessage, i.getName() + ".length"));
-                    }else if(count instanceof AtLeast){
-                        c.addStatement(CodeBlock.of("$T.checkArgument($L.length >= $L, $S, $L)", Preconditions.class, i.getName(), ((AtLeast) count).getMin(), errorMessage, i.getName() + ".length"));
-                    }else if(count instanceof AtMost){
-                        c.addStatement(CodeBlock.of("$T.checkArgument($L.length <= $L, $S, $L)", Preconditions.class, i.getName(), ((AtMost) count).getMax(), errorMessage, i.getName() + ".length"));
-                    }else if(count instanceof Range){
-                        c.addStatement(CodeBlock.of("$T.checkArgument($L.length >= $L && $L.length <= $L, $S, $L)", Preconditions.class, i.getName(), ((Range) count).getFrom(), i.getName(), ((Range) count).getTo(), errorMessage, i.getName() + ".length"));
-                    }
-                }
             }
         }
 
-        //Args:
         List<Arg> args = op.getArgs();
-        Map<DataType, Class> typeMapping = new HashMap<>();
-        typeMapping.put(DataType.BOOL, boolean.class);
-        typeMapping.put(DataType.FLOATING_POINT, double.class);
-        typeMapping.put(DataType.NUMERIC, double.class);
-        typeMapping.put(DataType.INT, long.class);
-
-        Map<DataType, Class> arrayTypeMapping = new HashMap<>();
-        arrayTypeMapping.put(DataType.BOOL, boolean[].class);
-        arrayTypeMapping.put(DataType.FLOATING_POINT, double[].class);
-        arrayTypeMapping.put(DataType.NUMERIC, double[].class);
-        arrayTypeMapping.put(DataType.INT, long[].class);
-
         if(args != null && !args.isEmpty()){
-            final int size = args.size();
-            for (int i = 0; i < size; i++) {
-                Arg arg = args.get(i);
-                inNames.add(arg.getName());
-                if (arg.getCount() == null || (arg.getCount() instanceof Exactly && ((Exactly) arg.getCount()).getCount() == 1)) {
-                    c.addParameter(typeMapping.get(arg.getType()), arg.getName());
+            for (Arg arg : args) {
+                final Count count = arg.getCount();
+                if (count == null || count.equals(exactlyOne)) {
                     c.addJavadoc("@param " + arg.getName() + " " + (arg.getDescription() == null ? "" : DocTokens.processDocText(arg.getDescription(), op)) + "\n");
                 } else {
-                    final Count count = arg.getCount();
                     c.addJavadoc("@param " + arg.getName() + " " + (arg.getDescription() == null ? "" : DocTokens.processDocText(arg.getDescription(), op)) + " (Size: " + count.toString() + ")\n");
-                    c.addParameter(arrayTypeMapping.get(arg.getType()), arg.getName());
-
-                    final String errorMessage = arg.getName() + " has incorrect size/length. Expected: " + count.toString() + ", got %s";
-                    if (count instanceof Exactly) {
-                        c.addStatement(CodeBlock.of("$T.checkArgument($L.length == $L, $S, $L)", Preconditions.class, arg.getName(), ((Exactly) count).getCount(), errorMessage, arg.getName()));
-                    } else if (count instanceof AtLeast) {
-                        c.addStatement(CodeBlock.of("$T.checkArgument($L.length >= $L, $S, $L)", Preconditions.class, arg.getName(), ((AtLeast) count).getMin(), errorMessage, arg.getName()));
-                    } else if (count instanceof AtMost) {
-                        c.addStatement(CodeBlock.of("$T.checkArgument($L.length <= $L, $S, $L)", Preconditions.class, arg.getName(), ((AtMost) count).getMax(), errorMessage, arg.getName()));
-                    } else if (count instanceof Range) {
-                        c.addStatement(CodeBlock.of("$T.checkArgument($L.length >= $L && $L.length <= $L, $S, $L)", Preconditions.class, arg.getName(), ((Range) count).getFrom(), arg.getName(), ((Range) count).getTo(), errorMessage, arg.getName()));
-                    }
                 }
             }
         }
@@ -212,13 +160,72 @@ public class Nd4jNamespaceGenerator {
                 throw new UnsupportedOperationException("Javadoc for multi-output ops not yet implemented");
             }
         }
+    }
 
+    @NotNull
+    private static List<String> buildParameters(MethodSpec.Builder c, Op op) {
+        //Inputs:
+        List<Input> in = op.getInputs();
+        List<String> inNames = new ArrayList<>();
+        if(in != null && !in.isEmpty()){
+            for(Input i : in){
+                final String inputName = i.getName();
+                inNames.add(inputName);
+
+                final Count count = i.getCount();
+                if(count == null || count.equals(exactlyOne)) {
+                    //Single input
+                    c.addParameter(INDArray.class, inputName);
+                } else {
+                    //Array input
+                    c.addParameter(INDArray[].class, inputName);
+                }
+                // Check for parameter types
+                c.addStatement(CodeBlock.of("$T.$L($S, $L)", NDValidation.class, validationMapping.get(i.getType()), op.getOpName(), inputName));
+                checkParameterCount(c, count, inputName);
+            }
+        }
+
+        //Args:
+        List<Arg> args = op.getArgs();
+
+        if(args != null && !args.isEmpty()){
+            for (Arg arg : args) {
+                final String argName = arg.getName();
+                inNames.add(argName);
+
+                final Count count = arg.getCount();
+                if (count == null || count.equals(exactlyOne)) {
+                    // single arg
+                    c.addParameter(typeMapping.get(arg.getType()), argName);
+                } else {
+                    // array Arg
+                    c.addParameter(arrayTypeMapping.get(arg.getType()), argName);
+                }
+
+                checkParameterCount(c, count, argName);
+            }
+        }
+        return inNames;
+    }
+
+    private static void buildConstraints(MethodSpec.Builder c, Op op) {
         // Constraints:
+        final List<Constraint> constraints = op.getConstraints();
         if(constraints != null && !constraints.isEmpty()){
             // Don't materialize the Backend Constraints
             for (Constraint constraint : constraints.stream().filter(it -> !(it instanceof BackendConstraint)).collect(Collectors.toList())) {
                 c.addStatement(CodeBlock.of("$T.checkArgument($L, $S)", Preconditions.class, constraintCodeGenerator.generateExpression(constraint.getCheck()), constraint.getMessage()));
             }
+        }
+    }
+
+    private static void buildExecution(MethodSpec.Builder c, Op op, List<String> inNames) {
+        boolean singleOut = op.getOutputs().size() == 1;
+        if(singleOut){
+            c.returns(INDArray.class);
+        } else {
+            c.returns(INDArray[].class);
         }
 
         //Op execution:
@@ -234,7 +241,46 @@ public class Nd4jNamespaceGenerator {
             sb.append("[0]");
 
         c.addStatement(sb.toString(), Nd4j.class);
+    }
 
-        return c.build();
+    private static void enableVarargsOnLastArg(MethodSpec.Builder c, Op op) {
+        if(op.getArgs() != null && !op.getArgs().isEmpty()){
+            final Arg lastArg = op.getArgs().get(op.getArgs().size() - 1);
+            final Count count = lastArg.getCount();
+            if(count != null && !count.equals(exactlyOne)){
+                c.varargs(true);
+            }
+        }
+    }
+
+    private static String countToJava(Count count,String paramName) {
+        final String paramLength = paramName + ".length";
+        if(count instanceof Exactly){
+            return paramLength + " == " + ((Exactly) count).getCount();
+        }else if(count instanceof AtLeast){
+            return paramLength + " >= " + ((AtLeast) count).getMin();
+        }else if(count instanceof AtMost){
+            return paramLength + " <= "+ ((AtMost) count).getMax();
+        }else if(count instanceof Range){
+            return ((Range) count).getFrom() + " <= " + paramLength + " && " + paramLength + " <= " + ((Range) count).getTo();
+        }else{
+            throw new IllegalArgumentException("Can not deal with Count of type " + count.getClass().getName());
+        }
+    }
+
+    private static void checkParameterCount(MethodSpec.Builder c, Count count, String paramName) {
+        // Check for parameter counts
+        if(count != null && !count.equals(exactlyOne)){
+            final String errorMessage = paramName + " has incorrect size/length. Expected: " + countToJava(count, paramName) + ", got %s";
+            if(count instanceof Exactly){
+                c.addStatement(CodeBlock.of("$T.checkArgument($L.length == $L, $S, $L)", Preconditions.class, paramName, ((Exactly) count).getCount(), errorMessage, paramName + ".length"));
+            }else if(count instanceof AtLeast){
+                c.addStatement(CodeBlock.of("$T.checkArgument($L.length >= $L, $S, $L)", Preconditions.class, paramName, ((AtLeast) count).getMin(), errorMessage, paramName + ".length"));
+            }else if(count instanceof AtMost){
+                c.addStatement(CodeBlock.of("$T.checkArgument($L.length <= $L, $S, $L)", Preconditions.class, paramName, ((AtMost) count).getMax(), errorMessage, paramName + ".length"));
+            }else if(count instanceof Range){
+                c.addStatement(CodeBlock.of("$T.checkArgument($L.length >= $L && $L.length <= $L, $S, $L)", Preconditions.class, paramName, ((Range) count).getFrom(), paramName, ((Range) count).getTo(), errorMessage, paramName + ".length"));
+            }
+        }
     }
 }
