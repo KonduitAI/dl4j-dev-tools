4009cda1fcb9bba192c82d26f043664dd5c54649
Paul Dubs <paul.dubs@gmail.com>
2019-11-26 11:14:19 +0100

    Add data types for data type, condition and loss reduce
    Signed-off-by: Paul Dubs <paul.dubs@gmail.com>

--- codegen/src/main/kotlin/org/nd4j/codegen/api/DataType.kt
+++ codegen/src/main/kotlin/org/nd4j/codegen/api/DataType.kt
@@ -4,5 +4,8 @@ enum class DataType {
     FLOATING_POINT, // Any floating point data type
     INT, // any integer data type
     NUMERIC, // any floating point or integer data type
-    BOOL // boolean data type
+    BOOL, // boolean data type
+    DATA_TYPE, // tensor data type
+    CONDITION, // A condition
+    LOSS_REDUCE // Loss reduction mode
 }
