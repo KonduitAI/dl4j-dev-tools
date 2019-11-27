d6d89f1af54657bab7fcba383fa0ed54489e2b48
Paul Dubs <paul.dubs@gmail.com>
2019-11-27 07:24:17 +0100

    Add namespace construction tests
    Namespace construction tests are meant to ensure that there are no
    exceptions thrown at run-time during the construction of a namespace.
    
    The usual reason for such an exception to occur is that an Op has been
    defined improperly.
    
    Signed-off-by: Paul Dubs <paul.dubs@gmail.com>

---
+++ codegen/src/test/kotlin/org/nd4j/codegen/ops/ConstructionTest.kt
@@ -0,0 +1,21 @@
+package org.nd4j.codegen.ops
+
+import org.junit.jupiter.api.Test
+
+/**
+ * Test that each Namespace actually constructs properly.
+ *
+ * This is allows us to utilize run-time consistency checks during the build process - if tests are enabled.
+ */
+class ConstructionTest {
+
+    @Test
+    fun bitwise() { Bitwise() }
+
+    @Test
+    fun random() { Random() }
+
+    @Test
+    fun math() { SDMath() }
+
+}
