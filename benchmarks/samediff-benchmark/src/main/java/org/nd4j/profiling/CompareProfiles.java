062833f71bc8c1a09d42c3059710757f2901ebb7
Alex Black <blacka101@gmail.com>
2019-12-19 20:09:57 +1100

    Cleanup and improvements
    Signed-off-by: Alex Black <blacka101@gmail.com>

--- benchmarks/samediff-benchmark/src/main/java/org/nd4j/profiling/CompareProfiles.java
+++ benchmarks/samediff-benchmark/src/main/java/org/nd4j/profiling/CompareProfiles.java
@@ -1,5 +1,6 @@
 package org.nd4j.profiling;
 
+import lombok.extern.slf4j.Slf4j;
 import org.nd4j.autodiff.listeners.profiler.ProfilingListener;
 import org.nd4j.autodiff.listeners.profiler.comparison.ProfileAnalyzer;
 import org.nd4j.autodiff.samediff.SameDiff;
@@ -13,13 +14,14 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 public class CompareProfiles {
 
     public static void main(String[] args) {
 
-        File modelsRootDir = new File("~/TF_Graphs/");
-        File tfProfileRootDir = new File("~/DL4J/dl4j-dev-tools/import-tests/profiling");
-        File sdProfileOutputDir = new File("~/DL4J/dl4j-dev-tools/import-tests/profiling/sd");
+        File modelsRootDir = new File("/home/alex/TF_Graphs/");
+        File tfProfileRootDir = new File("/home/alex/profiling");
+        File sdProfileOutputDir = new File("/home/alex/profiling/sd");
 
         //Available tests (so far): mobilenetv2, inception_resnet_v2, faster_rcnn_resnet101_coco
         String testName = "mobilenetv2";
@@ -36,19 +38,19 @@ public class CompareProfiles {
         switch (testName){
             case "mobilenetv2":
                 modelFile = new File(modelsRootDir, "mobilenet_v2_1.0_224_frozen.pb");
-                outputNames = Collections.singletonList("MobilenetV2/Predictions/Reshape_1:0");
-                ph = Collections.singletonMap("input:0", Nd4j.rand(DataType.FLOAT, batch, 224, 224, 3));
+                outputNames = Collections.singletonList("MobilenetV2/Predictions/Reshape_1");
+                ph = Collections.singletonMap("input", Nd4j.rand(DataType.FLOAT, batch, 224, 224, 3));
                 dirName = "mobilenet_v2_1.0_224_batch" + batch + "_tf-" + tfVersion;
                 break;
             case "inception_resnet_v2":
                 modelFile = new File(modelsRootDir, "mobilenet_v2_1.0_224_frozen.pb");
-                outputNames = Collections.singletonList("InceptionResnetV2/AuxLogits/Logits/BiasAdd:0");
-                ph = Collections.singletonMap("input:0", Nd4j.rand(DataType.FLOAT, batch, 299, 299, 3));
+                outputNames = Collections.singletonList("InceptionResnetV2/AuxLogits/Logits/BiasAdd");
+                ph = Collections.singletonMap("input", Nd4j.rand(DataType.FLOAT, batch, 299, 299, 3));
                 dirName = "inception_resnet_v2_batch" + batch + "_tf-" + tfVersion;
                 break;
             case "faster_rcnn_resnet101_coco":
                 modelFile = new File(modelsRootDir, "mobilenet_v2_1.0_224_frozen.pb");
-                outputNames = Arrays.asList("detection_boxes:0", "detection_scores:0", "num_detections:0", "detection_classes:0");
+                outputNames = Arrays.asList("detection_boxes", "detection_scores", "num_detections", "detection_classes");
                 ph = Collections.singletonMap("input:0", Nd4j.rand(DataType.FLOAT, batch, 600, 600, 3));
                 dirName = "faster_rcnn_resnet101_coco_batch" + batch + "_tf-" + tfVersion;
                 break;
@@ -60,34 +62,49 @@ public class CompareProfiles {
 
         SameDiff sd = SameDiff.importFrozenTF(modelFile);
 
+        log.info("Starting warmup - {} iterations", warmup);
         for( int i=0; i<warmup; i++ ){
             Map<String,INDArray> m = sd.output(ph, outputNames);
             for(INDArray arr : m.values()){
-                arr.close();
+                if(arr.closeable())
+                    arr.close();
             }
         }
 
 
         File sdDir = new File(sdProfileOutputDir, testName + "_" + System.currentTimeMillis());
-        ProfilingListener l = ProfilingListener.builder(sdDir)
+        sdDir.mkdirs();
+        File sdProfileFile = new File(sdDir, "profile.json");
+        ProfilingListener l = ProfilingListener.builder(sdProfileFile)
                 .recordAll()
                 .warmup(0)
                 .build();
         sd.setListeners(l);
 
 
+        log.info("Starting profiling - {} iterations", runs);
         for( int i=0; i<runs; i++ ){
             Map<String,INDArray> m = sd.output(ph, outputNames);
             for(INDArray arr : m.values()){
-                arr.close();
+                if(arr.closeable())
+                    arr.close();
             }
         }
 
         //Now, compare profiles:
         String s = ProfileAnalyzer.compareProfiles(sdDir, dir, ProfileAnalyzer.ProfileFormat.SAMEDIFF, ProfileAnalyzer.ProfileFormat.TENSORFLOW,
-                true, true, "sd", "tf", ProfileAnalyzer.SortBy.PROFILE1_PC);
+                true, true, "sd", "tf", ProfileAnalyzer.SortBy.RATIO);
 
         System.out.println(s);
+
+
+        System.out.println("============================================================================");
+        System.out.println(" ----- TensorFlow Profile Summary -----");
+        ProfileAnalyzer.summarizeProfileDirectory(dir, ProfileAnalyzer.ProfileFormat.TENSORFLOW);
+
+        System.out.println("============================================================================");
+        System.out.println(" ----- SameDiff Profile Summary -----");
+        ProfileAnalyzer.summarizeProfileDirectory(sdDir, ProfileAnalyzer.ProfileFormat.SAMEDIFF);
     }
 
 }
